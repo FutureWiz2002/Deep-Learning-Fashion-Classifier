5fc1aa474f8e037a274657209d1639866b029760
Jabir Mustahid <jabirm2002@gmail.com>
2024-03-01 21:09:22 -0500

    updated models

--- Model/variables/read.py
+++ Model/variables/read.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from keras.models import load_model
 
-model = create_model()
-model.load_weight("checkpoint.ckpt")
+
+saved_model = load_model('Model/model weights.h5')
+print("Model Loaded")
